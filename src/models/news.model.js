2a857db7f2a634868349cd1e27b3c2a700cb1c4a
javier muñoz <x_jemd_x@hotmail.com>
2021-05-12 21:34:54 -0400

    scrapping funcionando con error de codificación (no utf8)

--- src/models/news.model.js
+++ src/models/news.model.js
@@ -2,20 +2,31 @@ const cheerio = require('cheerio'); // librería para hacer scrapping
 const fetch = require('sync-fetch'); // librería para leer la web
 let newsModel = {};
 
-const url = "https://www.tarreo.com";
+const url = "https://vandal.elespanol.com";
 
 newsModel.getNews = async () => {
-  const $ = cheerio.load(fetch(url + '/noticias').text());
+
+  /**
+   * el html viene en una codificación diferente, por lo que
+   * primero decodificamos lo descargado para eliminar cualquier
+   * caracter raro
+  **/
+  let html = fetch(url + '/noticias/videojuegos').arrayBuffer();
+  const decoder = new TextDecoder('windows-1252');
+  html = decoder.decode(html)
+
+  // ahora cargamos lo leído a cheerio
+  const $ = cheerio.load(html);
   let news = [];
   
   // por cada noticia obtenemos su titulo, descripción y fecha de publicación
-  $('div.content').has('.elementHeader').has('p').map((i, el) => {
+  $('.caja620').map((i, el) => {
     news.push({
       "uid": `NOTICIA_${i+1}`,
-      "updateDate": $(el).find('time').attr('datetime'),
-      "titleText": $(el).find('a.rC').attr('title'),
-      "mainText": $(el).find('a.rC').attr('title').trim() + '. ' + $(el).find('.elementIntro').text().trim(),
-      "redirectionUrl": url + $(el).find('.tc').find('a').attr('href').split('#')[0]
+      "updateDate": new Date($(el).find('.timestamp_fecha').attr('data-timestamp') * 1000),
+      "titleText": $(el).find('a').text().trim(),
+      "mainText": $(el).find('a').text().trim() + '. ' + $(el).find('.desccaja').text().trim(),
+      "redirectionUrl": $(el).find('a').attr('href')
      })
   })
 
